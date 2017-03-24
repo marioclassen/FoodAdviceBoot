c5f1f0d87d085b58bedd9a84817ab7ccf7860d65
Michael Brunner <mbrunner.kn@gmail.com>
2017-03-24 09:50:39 +0100

    added some more intents

--- index-ai.js
+++ index-ai.js
@@ -60,6 +60,34 @@ intents.matches('CanIEatThis', [
     }
 ]);
 
+intents.matches('RestaurantAdvice', [
+    function (session) {
+        session.send("I can recommend you the El Nacional");
+    }
+]);
+
+//TODO: implement logic to parse all user date
+intents.matches('UserProfile', [
+    function (session, args) {
+        var givenName = builder.EntityRecognizer.findEntity(args.entities, 'givenName');
+        if (givenName) {
+            var given_name = givenName.entity;
+
+            // to include API Calls
+            //var url = "http://api.apixu.com/v1/current.json?key=<Your API Key>&q=" + city_name;
+            //request(url,function(error,response,body){
+            //    body = JSON.parse(body);
+            //    temp = body.current.temp_c;
+
+            session.send("Hello (givenName) " + given_name);
+        } else {
+            //builder.Prompts.text(session, 'Which food do you want me to check?');
+        }
+    },
+    function (session, results) {
+        session.send("I will check if you can eat " + results.response);
+    }
+]);
 
 intents.onDefault(function (session) {
     session.send("Sorry...can you please rephrase?");
